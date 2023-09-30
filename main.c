2c75375dab1cb840cf413c15042efa60a0588a37
Ondrej Lukasek <ondrej@lukasek.cz>
2023-09-30 21:39:29 +0200

    correctly checking IP and overwriting capacity

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <pcap.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
-#include <arpa/inet.h>  // For inet_ntoa()
 #include <netinet/if_ether.h>
 #include <time.h>
 
@@ -50,6 +50,7 @@ struct dhcp_packet {
     char file[128];         // Boot file name, null terminated string; "generic" name or null in DHCPDISCOVER, fully qualified directory-path name in DHCPOFFER.
     u_int8_t options[312];  // Optional parameters field. See RFC 2132.
 };
+
 
 // Occupied IP address structure
 struct occAddr {
@@ -193,7 +194,89 @@ void print_dhcp_packet(const struct dhcp_packet* dhcp) {
     printf("\n");
 }
 
-void packet_handler(u_char* user_data, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
+typedef struct {
+    struct in_addr ip;
+    int prefix;
+    int dev_count;
+} IP_Prefix;
+
+typedef struct {
+    IP_Prefix *prefixes;
+    int count;
+} IP_Prefixes;
+
+IP_Prefixes ip_prefixes;
+
+int parse_ip_prefix(const char *str, IP_Prefix *prefix) {
+    char ip_str[INET_ADDRSTRLEN];
+    char *slash = strchr(str, '/');
+    if (!slash) return 0;  // Rozsah nema validni format
+
+    // Ziskani IP adresy z rozsahu
+    strncpy(ip_str, str, slash - str);
+    ip_str[slash - str] = '\0';
+    if (inet_pton(AF_INET, ip_str, &prefix->ip) <= 0) return 0;  // Chyba pri konverzi IP adresy
+
+    // Ziskani prefixu
+    prefix->prefix = atoi(slash + 1);
+    if (prefix->prefix <= 0 || prefix->prefix > 32) return 0;  // Neplatna delka prefixu
+
+    prefix->dev_count = 0;
+
+    return 1;
+}
+
+void print_ip_ranges(const IP_Prefixes *ip_prefixes) {
+    char ip_str[INET_ADDRSTRLEN];  // Prostor pro uložení řetězcové reprezentace IP adresy
+
+    for (int i = 0; i < ip_prefixes->count; i++) {
+        if (inet_ntop(AF_INET, &ip_prefixes->prefixes[i].ip, ip_str, INET_ADDRSTRLEN)) {
+            int max_devs = (1 << (32 - ip_prefixes->prefixes[i].prefix)) - 2;
+            int dev_count = ip_prefixes->prefixes[i].dev_count;
+            printf("IP rozsah: %s/%d %d %d %.2f%%        \n", ip_str, ip_prefixes->prefixes[i].prefix, max_devs, dev_count, (double) dev_count*100/max_devs);
+        } else {
+            printf("Chyba při konverzi IP adresy pro index %d.\n", i);
+        }
+    }
+    for (int i = 0; i < ip_prefixes->count; i++) {
+        printf("\033[A");
+    }
+}
+
+uint32_t ip_to_uint32(const struct in_addr *ip) {
+    return ntohl(ip->s_addr);
+}
+
+int is_ip_in_prefix(const struct in_addr *ip, const IP_Prefix *prefix) {
+    uint32_t ip_val = ip_to_uint32(ip);
+    uint32_t prefix_val = ip_to_uint32(&prefix->ip);
+
+    // Vytvoření masky podle prefixu
+    uint32_t mask = prefix->prefix == 0 ? 0 : (~0) << (32 - prefix->prefix);
+
+    // Zjistí, jestli IP adresa patří do rozsahu
+    return (ip_val & mask) == (prefix_val & mask);
+}
+
+void update_dev_count(struct occAddr *head, IP_Prefixes *ip_prefixes) {
+    // Nejdříve inicializujeme čítadla na nulu
+    for (int i = 0; i < ip_prefixes->count; i++) {
+        ip_prefixes->prefixes[i].dev_count = 0;
+    }
+
+    // Projdeme všechny IP adresy v seznamu
+    struct occAddr *current = head;
+    while (current) {
+        for (int i = 0; i < ip_prefixes->count; i++) {
+            if (is_ip_in_prefix(&current->ip, &ip_prefixes->prefixes[i])) {
+                (ip_prefixes->prefixes[i].dev_count)++;
+            }
+        }
+        current = current->next;
+    }
+}
+
+void packet_handler(unsigned char* user_data, const struct pcap_pkthdr* pkthdr, const unsigned char* packet) {
     struct ip* ip_header = (struct ip*)(packet + 14);
     struct udphdr* udp_header = (struct udphdr*)(packet + 14 + (ip_header->ip_hl << 2));
     struct dhcp_packet* dhcp_pkt = (struct dhcp_packet*)(packet + 14 + (ip_header->ip_hl << 2) + sizeof(struct udphdr));
@@ -201,7 +284,8 @@ void packet_handler(u_char* user_data, const struct pcap_pkthdr* pkthdr, const u
     if (ntohs(udp_header->source) == DHCP_SERVER_PORT || ntohs(udp_header->source) == DHCP_CLIENT_PORT ||
         ntohs(udp_header->dest) == DHCP_SERVER_PORT || ntohs(udp_header->dest) == DHCP_CLIENT_PORT) {
         // printf("DHCP packet captured\n");
-        printf("DHCP packet received from %s to %s\n", inet_ntoa(ip_header->ip_src), inet_ntoa(ip_header->ip_dst));
+        // printf("DHCP packet received from %s to %s\n", inet_ntoa(ip_header->ip_src), inet_ntoa(ip_header->ip_dst));
+        print_ip_ranges(&ip_prefixes);  
         // print_dhcp_packet(dhcp_pkt);
 
         time_t expTime;
@@ -236,9 +320,12 @@ void packet_handler(u_char* user_data, const struct pcap_pkthdr* pkthdr, const u
                     }
                     if (dhcp_type == DHCP_RELEASE)
                     {
-                        printf("Releasing IP: %s\n", inet_ntoa(dhcp_pkt->ciaddr));
+                        // printf("Releasing IP: %s\n", inet_ntoa(dhcp_pkt->ciaddr));
                         removeElement(dhcp_pkt->ciaddr);   
-                        printf("items in list: %ld\n", countElements());     
+                        // printf("items in list: %ld\n", countElements()); 
+                        print_ip_ranges(&ip_prefixes);    
+                        update_dev_count(head, &ip_prefixes);
+                        print_ip_ranges(&ip_prefixes);    
                     }
                     break;
                 case DHCP_OPTION_LEASE_TIME:
@@ -321,43 +408,126 @@ void packet_handler(u_char* user_data, const struct pcap_pkthdr* pkthdr, const u
                     current = current->next;
                 }
             }
-            printf("items in list: %ld\n", countElements());
+            print_ip_ranges(&ip_prefixes);    
+            update_dev_count(head, &ip_prefixes);
+            print_ip_ranges(&ip_prefixes);    
+            // printf("items in list: %ld\n", countElements());
         }
         // printf("-----------\n");
     }
 }
 
+
+
 int main(int argc, char* argv[]) {
-    pcap_if_t* alldevs, * d, *device;
+    pcap_if_t* alldevs = NULL;
     char errbuf[PCAP_ERRBUF_SIZE];
     errbuf[0] = 0;
     pcap_t* handle;
+    char *filename = NULL;
+    char *interface = NULL;
 
-    printf("Tohle je breakpoint\n");
+    ip_prefixes.count = 0;
+    ip_prefixes.prefixes = NULL;
+    // TIMHLE SI NEJSEM JISTEJ
+    extern char *optarg;
+    extern int optind, opterr, optopt;
 
-    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-        fprintf(stderr, "Error finding devices: %s\n", errbuf);
+    int opt;
+    while ((opt = getopt(argc, argv, "r:i:")) != -1) {
+        switch (opt) {
+            case 'r':
+                filename = optarg;
+                if (access(filename, F_OK) == -1) {
+                    fprintf(stderr, "Vstupní soubor '%s' neexistuje.\n", filename);
+                    return 1;
+                }
+                break;
+            case 'i':
+                interface = optarg;
+                break;
+            default:
+                fprintf(stderr, "Neznámá volba: %c\n", opt);
+                return 1;
+        }
+    }
+
+    if (!filename && !interface) {
+        fprintf(stderr, "Chyba: Musíte zadat buď -r <filename> nebo -i <interface-name>\n");
         return 1;
     }
 
-    // If no device found, exit
-    if (alldevs == NULL) {
-        fprintf(stderr, "No devices found\n");
+    if (interface) {
+        pcap_if_t *alldevs;
+        char errbuf[PCAP_ERRBUF_SIZE];
+        if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+            fprintf(stderr, "Chyba v pcap_findalldevs: %s\n", errbuf);
+            return 1;
+        }
+
+        pcap_if_t *device;
+        int found = 0;
+        for (device = alldevs; device; device = device->next) {
+            if (strcmp(device->name, interface) == 0) {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            fprintf(stderr, "Rozhraní '%s' nenalezeno.\n", interface);
+            return 1;
+        }
+
+        // pcap_freealldevs(alldevs);
+    }
+
+    if (optind == argc) {
+        fprintf(stderr, "Chybí IP prefix(y) pro generování statistiky\n");
         return 1;
     }
 
-    // Use the first device (you can modify this to select a specific device)
-    d = alldevs;
-    printf("interface: %s\n", d->name);
-
-    // Print each device name
-    for (device = alldevs; device; device = device->next) {
-        printf("Name: %s, %s\n", device->name, device->description);
+    // Uložení všech IP prefixů do dynamického pole
+    ip_prefixes.count = argc - optind;
+    ip_prefixes.prefixes = (IP_Prefix *) malloc(ip_prefixes.count * sizeof(IP_Prefix));
+    if (ip_prefixes.prefixes == NULL) {
+        fprintf(stderr, "Chyba při alokaci paměti.\n");
+        return 1;
+    }
+    
+    for (int i = 0; optind < argc; optind++, i++) {
+        if (!parse_ip_prefix(argv[optind], &ip_prefixes.prefixes[i])) {
+            fprintf(stderr, "Chyba při parsování IP prefixu: %s\n", argv[optind]);
+            free(ip_prefixes.prefixes);
+            return 1;
+        }
     }
 
-    handle = pcap_open_live(d->name, BUFSIZ, 1, 1000, errbuf);
+    printf("Filename: %s\n", filename ? filename : "Není zadán");
+    printf("Interface: %s\n", interface ? interface : "Není zadán");
+    
+    // for (int i = 0; i < ip_prefixes.count; i++) {
+    //     char ip_str[INET_ADDRSTRLEN];  // Prostor pro uložení řetězcové reprezentace IP adresy
+    //     printf("IP prefix: %s\n", inet_ntop(AF_INET, ip_prefixes->prefixes[i].ip, ip_str, INET_ADDRSTRLEN));
+    // }
+
+    print_ip_ranges(&ip_prefixes);
+    print_ip_ranges(&ip_prefixes);
+ 
+    // printf("Tohle je breakpoint\n");
+
+    // // Use the first device (you can modify this to select a specific device)
+    // d = alldevs;
+    // printf("interface: %s\n", d->name);
+
+    // // Print each device name
+    // for (device = alldevs; device; device = device->next) {
+    //     printf("Name: %s, %s\n", device->name, device->description);
+    // }
+
+    handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "Error opening device %s: %s\n", d->name, errbuf);
+        fprintf(stderr, "Error opening device %s: %s\n", interface, errbuf);
         pcap_freealldevs(alldevs);
         return 2;
     }
@@ -366,5 +536,7 @@ int main(int argc, char* argv[]) {
 
     pcap_close(handle);
     pcap_freealldevs(alldevs);
+    free(ip_prefixes.prefixes);
+
     return 0;
 }
