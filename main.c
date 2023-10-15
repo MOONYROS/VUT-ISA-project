fd6a4f3eb6dcf9792af766f944552bd5a13fc4f4
Ondrej Lukasek <ondrej@lukasek.cz>
2023-10-15 14:22:52 +0200

    some more comments

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/**
+ * @file main.c
+ * @brief This is the main file of the project, it contains the main loop fo the project.
+ * @author Ondrej Lukasek (xlukas15)
+ * @date 2023-10
+ * @copyright MIT LICENSE
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -60,7 +68,11 @@ struct occAddr {
 
 struct occAddr *head = NULL;
 
-// Add a new item
+/**
+ * @brief Function adds element to the list of elements.
+ * @param ip Structure containing the IP address.
+ * @param tm Time duration for which the IP address stays in the list.
+*/
 void addElement(struct in_addr ip, time_t tm) {
     struct occAddr *newNode = malloc(sizeof(struct occAddr));
     newNode->ip = ip;
@@ -69,7 +81,11 @@ void addElement(struct in_addr ip, time_t tm) {
     head = newNode;
 }
 
-// Find an item by IP
+/**
+ * @brief Function searches for certain IP address in the list.
+ * @param ip Structure containing the IP address.
+ * @return Returns NULL if the item is not found. On succes returns the IP address structure.
+*/
 struct occAddr* findElement(struct in_addr ip) {
     struct occAddr *current = head;
     while (current) {
@@ -81,7 +97,12 @@ struct occAddr* findElement(struct in_addr ip) {
     return NULL;
 }
 
-// Update an item's time
+/**
+ * @brief Function updates the IP address duration.
+ * @param ip Structure containing the IP address.
+ * @param tm New time duration for which the IP address stays in the list.
+ * @return Returns 1 on successful update and 0 after failed attempt to find the element in the list. 
+*/
 int updateElement(struct in_addr ip, time_t tm) {
     struct occAddr *node = findElement(ip);
     if (node) {
@@ -91,7 +112,11 @@ int updateElement(struct in_addr ip, time_t tm) {
     return 0;  // Not found
 }
 
-// Remove an item by IP
+/**
+ * @brief Function removes an IP address from the list.
+ * @param ip Structure containing the IP address.
+ * @return Returns 1 on successful element removal and 0 after failed attempt to find the element in the list.
+*/
 int removeElement(struct in_addr ip) {
     struct occAddr *current = head;
     struct occAddr *prev = NULL;
