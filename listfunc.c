32715c41ab1e4c86a079d6dd117f33c764cf29b5
Ondrej Lukasek <ondrej@lukasek.cz>
2023-10-28 16:28:04 +0200

    removed comments

--- listfunc.c
+++ listfunc.c
@@ -1,11 +1,5 @@
-// #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
 #include <pcap.h>
-// #include <netinet/ip.h>
-// #include <netinet/udp.h>
-// #include <netinet/if_ether.h>
 #include <time.h>
 
 #include "common.h"
